eec024c151991ef327d570be4727b0b318e09e70
Kalyani Girgune <155078937+kalyanigirgune@users.noreply.github.com>
2025-04-03 18:47:47 +0530

    Update markAttendanceRoutes.js

--- routes/markAttendanceRoutes.js
+++ routes/markAttendanceRoutes.js
@@ -3,25 +3,30 @@ const router = express.Router();
 const Registration = require("../models/Registration");
 const mongoose = require("mongoose");
 
+// Route to render the attendance page
 router.get("/markAttendance/:conferenceId", (req, res) => {
     res.render("markAttendance", { conferenceId: req.params.conferenceId });
 });
 
+// Route to mark attendance
 router.post("/markAttendance/:conferenceId", async (req, res) => {
     try {
         const { email } = req.body;
         let { conferenceId } = req.params;
 
+        // Convert conferenceId to ObjectId if it's valid
         if (mongoose.Types.ObjectId.isValid(conferenceId)) {
             conferenceId = new mongoose.Types.ObjectId(conferenceId);
         }
 
+        // Find the registered user for the given conference
         const user = await Registration.findOne({ email, conferenceId });
 
         if (!user) {
             return res.status(404).json({ message: "❌ User not registered for this conference!" });
         }
 
+        // Mark attendance
         user.attended = true;
         await user.save();
 
@@ -33,4 +38,4 @@ router.post("/markAttendance/:conferenceId", async (req, res) => {
     }
 });
 
-module.exports = router; 
+module.exports = router;
